6c65645c2cc8bd3d79a8856f543002eec58419bc
JNeto <joaomendes15@gmail.com>
2024-09-19 05:40:49 +0100

    Improved scene management.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,14 @@ using Debug = UnityEngine.Debug;
 public class GameManager : MonoBehaviour
 {
     
+    private enum Difficulty
+    {
+        Easy,
+        Mid,
+        Hard,
+        Inferno
+    }
+    
     [Title("Gameplay")]   
     [SerializeField] private float _countdownDuration = 15.5f;
     [SerializeField] private float _displayEndGameMenuAfter = 3f;
@@ -18,6 +26,7 @@ public class GameManager : MonoBehaviour
     [Title("Debugging")] 
     [ReadOnly, SerializeField] private float _countdown = 0;
     [ReadOnly, SerializeField] private bool _isRunning = false;
+    [ReadOnly, SerializeField] private Difficulty _difficulty = Difficulty.Easy;
     
     [Title("References")]
     [SerializeField] private GameObject _musicManagerPrefab;
@@ -30,50 +39,83 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _countdownText; 
     [FormerlySerializedAs("_fishModelController")] [SerializeField] private FishCatchingController _fishCatchingController;
     
+    
+    private void Start()
+    {
+        _fishingUI.SetActive(false);
+        
+        // Instantiates a new MusicManager in case there is None in the scene.
+        // The MusicPlayer script also checks if it is the only instance in the scene.
+        if (FindObjectOfType<MusicPlayer>() is null)
+            Instantiate(_musicManagerPrefab);
+        
+        // sets up the initial difficulty
+        _difficulty = Difficulty.Easy;
+    }
+    
+    public void LoadLevel(int sceneIndex)
+    {
+        // The current scene shouldn't be loaded.
+        if (SceneManager.GetActiveScene().buildIndex == sceneIndex) 
+            return;
+
+        _difficulty = (Difficulty) sceneIndex;
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void RunNewGameAtCurrentDifficulty()
+    {
+        switch (_difficulty)
+        {
+            case Difficulty.Easy: RunNewGameEasy(); break;
+            case Difficulty.Mid: RunNewGameMid(); break;
+            case Difficulty.Hard: RunNewGameHard(); break;
+            case Difficulty.Inferno: RunNewGameInferno(); break;
+        }
+    }
+    
     [Button]                                                    
-    public void RunNewGameEasy()                                 
+    private void RunNewGameEasy()                                 
     {                                                           
         _fishingBars._fishTimerMultiplier = 1.5f;               
         _fishingBars._escapeBarIncrement = 0.2f;                
         _fishingBars._fishSmoothMotion = 0.7f;  
         _fishingBars._hookEscapeDecrement = 0.15f;     
-        RunNewGame(0);                                           
+        RunNewGame();                                           
     }                                                           
     
     [Button]                               
-    public void RunNewGameMid()
+    private void RunNewGameMid()
     {
         _fishingBars._fishTimerMultiplier = 1.3f;
         _fishingBars._escapeBarIncrement = 0.25f; 
         _fishingBars._fishSmoothMotion = 0.6f; 
         _fishingBars._hookEscapeDecrement = 0.125f;    
-        RunNewGame(1);
+        RunNewGame();
     }                                      
     
     [Button]                                              
-    public void RunNewGameHard()                           
+    private void RunNewGameHard()                           
     {                                                     
         _fishingBars._fishTimerMultiplier = 1f;
         _fishingBars._escapeBarIncrement = 0.3f;
         _fishingBars._fishSmoothMotion = 0.5f;
         _fishingBars._hookEscapeDecrement = 0.1f;
-        RunNewGame(2);                                     
+        RunNewGame();                                     
     }                                                     
                                                                        
     [Button]                                                       
-    public void RunNewGameInferno()                                   
+    private void RunNewGameInferno()                                   
     {                                                              
         _fishingBars._fishTimerMultiplier = 0.8f;                    
         _fishingBars._escapeBarIncrement = 0.4f;                   
         _fishingBars._fishSmoothMotion = 0.3f;                     
         _fishingBars._hookEscapeDecrement = 0.07f;                  
-        RunNewGame(0);                                              
+        RunNewGame();                                              
     }                                                              
     
-    private void RunNewGame(int sceneIndex)
+    private void RunNewGame()
     {
-        if (SceneManager.GetActiveScene().buildIndex != sceneIndex)
-            SceneManager.LoadScene(sceneIndex);
         _fishCatchingController.ResetFish();
         _fishingUI.SetActive(true);
         _newGamePanel.SetActive(false);
@@ -84,16 +126,6 @@ public class GameManager : MonoBehaviour
         _isRunning = true;
         _fishingBars.ResetTheBars();
         _fishingBars.CanRun = true;
-    }
-
-    private void Start()
-    {
-        _fishingUI.SetActive(false);
-        
-        // Instantiates a new MusicManager in case there is None in the scene.
-        // The MusicPlayer script also checks if it is the only instance in the scene.
-        if (FindObjectOfType<MusicPlayer>() is null)
-            Instantiate(_musicManagerPrefab);
     }
 
     // Update is called once per frame
